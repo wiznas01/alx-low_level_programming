30d21e61d55f13da1f444b8b5c9552ce1eb925e8
wiznas01 <wiznas666@gmail.com>
2023-09-07 15:08:43 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - the main function
+*
+* Return: returns the value 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

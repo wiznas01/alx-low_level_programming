9624175c26b1ae10b655d78eb3ee8cd4f98ebb6c
wiznas01 <wiznas666@gmail.com>
2023-09-07 15:43:44 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,5 +8,10 @@
 
 int main(void)
 {
-	printf()
+	printf("Size of a char: %lu byte(s)", sizeof(char));
+	printf("Size of an int: %lu byte(S)", sizeof(int));
+	printf("Size of a long int: %lu byte(S)" sizeof(long int));
+	printf("Size of a long long int: %lu byte(S)", sizeof(long long int));
+	printf("Size of a float: %lu byte(S)", sizeof(float));
+	return (0);
 }

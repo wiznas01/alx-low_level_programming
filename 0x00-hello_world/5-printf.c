9996d24e4ad480662cec50dac9bd17fb561bbb2f
wiznas01 <wiznas666@gmail.com>
2023-09-07 15:18:53 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function of main
+ *
+ * Return: returns the value 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

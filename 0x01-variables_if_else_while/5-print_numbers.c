2beed674d1f071b77bccad1e87bc2fdc3bc195c1
wiznas01 <wiznas666@gmail.com>
2023-09-08 12:46:47 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - the function of main
+ *
+ * Return: returns the value 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0 ; a < 10 ; a++)
+		printf("%d", a);
+	printf("\n");
+	return (0);
+}

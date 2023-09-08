87ac6c1d2e4f2983287d440744fb85d9979056af
wiznas01 <wiznas666@gmail.com>
2023-09-08 11:52:12 -0700

    print alphabets 2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - the function of main
+ *
+ * Return: returns the value 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}

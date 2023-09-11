a6ac95ba4e96a0a7d50b5d25062bb2ef993ff09a
wiznas01 <wiznas666@gmail.com>
2023-09-11 00:44:24 -0700

    new

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - the function main 
+ *
+ * Return: returns the value 0
+ */
+
+int main(void)
+{
+	const char("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
+	return (1);
+}

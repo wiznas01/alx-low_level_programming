d8437479f37ef3dde7515d0e6a4d72dd91f09530
wiznas01 <wiznas666@gmail.com>
2023-09-11 00:57:43 -0700

    new

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,14 @@
 #include <string.h>
 
 /**
- * main - the function main 
+ * main - the function main
  *
  * Return: returns the value 0
  */
 
 int main(void)
 {
-	const char("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
+	const char("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	printf("\n");
 	return (1);
 }

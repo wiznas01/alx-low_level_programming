8d2a33b2820822455d001a2c9891b22ffb4fbf8d
wiznas01 <wiznas666@gmail.com>
2023-09-12 15:15:59 -0700

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - function to print every minute of jack bauer day
+ *
+ * Return: returns the value 0
+ */
+
+void jack_bauer(void)
+{
+	int x, y;
+
+	for (x = 0 ; x < 24 ; x++)
+	{
+		for (y = 0 ; y < 60 ; y++)
+		{
+			_putchar(x / 10 + '0');
+			_putchar(x % 10 + '0');
+			_putchar(':');
+			_putchar(y / 10 + '0');
+			_putchar(y % 10 + '0');
+		}
+	}
+}

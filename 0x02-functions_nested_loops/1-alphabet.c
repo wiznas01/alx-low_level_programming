b1df16a806789ab509bac298adf83bde7024a706
wiznas01 <wiznas666@gmail.com>
2023-09-12 09:54:36 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function to print the alphabets
+ *
+ * Return: returns the value 0
+ */
+
+void print_alphabet(void)
+{
+	int m;
+
+	for (m = 'a' ; m <= 'z' ; m++)
+		_putchar(m);
+	_putchar('\n');
+}

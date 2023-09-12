1a0501bf20cb2df3ce03743ddfa95e0ad8d20bc1
wiznas01 <wiznas666@gmail.com>
2023-09-12 16:07:36 -0700

    multiplication table 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,12 +14,12 @@ void times_table(void)
 		for (y = 0 ; y < 10 ; y++)
 		{
 			z = x * y;
-			if (z == 0)
+			if (y == 0)
 				printf("%d, ", z);
 			else
 			{
-				printf("%2d, ", z);
-				if (z != 9)
+				printf("%2d", z);
+				if (y != 9)
 					printf(", ");
 
 			}

3cd92383c4bf75abb0f7e1ae7502267bfa570537
wiznas01 <wiznas666@gmail.com>
2023-09-12 17:08:45 -0700

    printing 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - function to print to the number 98
+ * @n: parameter to be checked
+ * Return: returns the value 0
+ */
+
+void print_to_98(int n)
+{
+	int m;
+
+	if (n >= 0 && n <= 98)
+	{
+		for (m = n ; m <= 98 ; m++)
+		{	
+			printf("%d", m);
+			if (m != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
+	else if (n < 0)
+	{
+		for (m = n ; m <= 98 ; m++)
+		{
+			printf("%d", m);
+			if (m != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		for (m = n ; m >= 98 ; m--)
+		{
+			printf("%d", m);
+			if (m != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
+	else if (n == 98)
+		printf("%d\n", n);
+}

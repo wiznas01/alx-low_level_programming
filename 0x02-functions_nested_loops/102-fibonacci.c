3fc5376be44165d16a9096855e4af906f7d615d7
wiznas01 <wiznas666@gmail.com>
2023-09-13 10:36:21 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - program to find the fibonacci sequence of numbers between 0 and 50
+ *
+ * Return: returns the value 0
+ */
+
+int main(void)
+{
+	int x = 1;
+	int y = 2;
+	int next;
+	int i;
+
+	printf("%d, ", x);
+	for (i = 1 ; i < 50 ; i++)
+	{
+		printf("%d", y);
+		next = x + y;
+		x = y;
+		y = next;
+	}
+	printf"\n");
+	return (0);
+}

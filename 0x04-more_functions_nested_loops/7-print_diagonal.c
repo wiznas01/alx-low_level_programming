13609d6f33e5986362d7184b2e7dd2cf858d2119
wiznas01 <wiznas666@gmail.com>
2023-09-14 07:56:48 -0700

    print diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function to print \ on the terminal
+ * @n: parameter to be checked
+ * Return: returns the value 0
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int x, y;
+
+		for (x = 0 ; x < n ; x++)
+		{
+			for (y = 0 ; y <= x; y++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}

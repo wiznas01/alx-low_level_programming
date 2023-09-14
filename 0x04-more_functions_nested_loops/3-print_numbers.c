01aafcb58958f9e709916d0801026167f5d54c78
wiznas01 <wiznas666@gmail.com>
2023-09-14 06:13:36 -0700

    print numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - function to print numbers 0 to 9
+ *
+ * Return: returns 0
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0 ; x < 10 ; x++)
+		_putchar(x + '0');
+	return (0);
+}

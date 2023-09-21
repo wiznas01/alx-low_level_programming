68a9f27f7770e13e3dbe8cbc2cefddce7772025c
wiznas01 <wiznas666@gmail.com>
2023-09-21 09:31:17 -0700

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -19,7 +19,7 @@ int _strcmp(char *s1, char *s2)
 			if (s1[i] < s2[i])
 				return (s1[i] - s2[i]);
 			else if (s1[i] > s2[i])
-				return (s2[i] - s1[i]);
+				return (s1[i] - s2[i]);
 		}
 	}
 	return (0);

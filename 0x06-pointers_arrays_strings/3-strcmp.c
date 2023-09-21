49ef30f140838a43a8c73ca5530c5a554a244bb4
wiznas01 <wiznas666@gmail.com>
2023-09-21 09:28:50 -0700

    compare string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,18 +10,17 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+	int i;
+
+	for (i = 0 ; s1[i] != '\0' || s2[i] != '\0' ; i++)
 	{
-		s1++;
-		s2++;
-	}
-	if (*s1 < *s2)
-	{
-		return (-15);
-	}
-	else if (*s1 > *s2)
-	{
-		return (15);
+		if (s1[i] != s2[i])
+		{
+			if (s1[i] < s2[i])
+				return (s1[i] - s2[i]);
+			else if (s1[i] > s2[i])
+				return (s2[i] - s1[i]);
+		}
 	}
 	return (0);
 }

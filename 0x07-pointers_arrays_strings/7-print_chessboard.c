d1176e8e136e46180de49cafa5e0e26207cb0a63
wiznas01 <wiznas666@gmail.com>
2023-09-25 14:49:10 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function to print chessboard
+ * @a: 8 of rows and columns
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < 8 ; i++)
+	{
+		for (j = 0 ; j < 8 ; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		_putchar('\n');
+	}
+}

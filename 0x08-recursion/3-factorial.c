b8c93a891c80419e65908e01da946141fd4cc627
wiznas01 <wiznas666@gmail.com>
2023-09-29 01:57:36 -0700

    print n!

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - function to print factorial
+ * @n: paramater n
+ * Return: returns the factorial of param n
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	return (n * factorial(n - 1));
+}

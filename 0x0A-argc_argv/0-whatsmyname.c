08c6d2e4acb73ceb63fc0723088ffab04ded7b57
wiznas01 <wiznas666@gmail.com>
2023-10-02 08:39:11 -0700

    print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - the function main to print program
+ * @argc: argument count
+ * @argv: argument value
+ * Return: returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}

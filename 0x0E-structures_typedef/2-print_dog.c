5c4fa43acd21f1ad1d3549f782e0168d14008a46
wiznas01 <wiznas666@gmail.com>
2023-10-10 10:22:26 -0700

    print a struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * print_dog - print dog
+ * @d: dog to print
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("name %s\n", (d->name) ? d->name : "(nil)");
+		printf("age %f\n", (d->age) ? d->age : 0);
+		printf("owner %s\n", (d->owner) ? d->owner : "(nil)");
+	}
+}

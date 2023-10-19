6aa95bb18f22944cb6e840ac90225f4e6608f2b9
wiznas01 <wiznas666@gmail.com>
2023-10-19 14:40:54 -0700

    print elements of a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - function to print all elements of a list
+ * @h: singly linked list
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t elements;
+
+	elements = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		elements++;
+	}
+	return (elements);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+C language - singly linked list

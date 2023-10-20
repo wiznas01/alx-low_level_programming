4a9c3839db4b6baefddea5ddb1f03b7ee6817055
wiznas01 <wiznas666@gmail.com>
2023-10-20 01:42:48 -0700

    print anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+/**
+ * print_all - function to print anything
+ * @format: format string containing format specifiers
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0;
+	char *str;
+	char *sep = "";
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", sep, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(args, double));
+				break;
+			case 's':
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					printf("%s(nil)", sep);
+				else
+					printf("%s%s", sep, str);
+				break;
+			}
+			default:
+			i++;
+			continue;
+		}
+		sep = ", ";
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}

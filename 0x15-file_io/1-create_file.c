ad9f387f51758cefc01bde103cb21cfe717f50f5
wiznas01 <wiznas666@gmail.com>
2023-10-31 15:14:09 -0700

    hello world

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * create_file - creates a file and writes text content to it
+ * @filename: name of the file to create
+ * @text_content: NULL-terminated string to write
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, write_status;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		write_status = write(fd, text_content, strlen(text_content));
+		if (write_status == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
